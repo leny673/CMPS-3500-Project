e4e5337b1b840e747fc48ebe036f976ce5a67058
leny <ecastaneda11@outlook.com>
2022-04-17 01:19:39 -0700

    All questions answered. TO DO: Create functions.

--- ClassProjectGroup1.py
+++ ClassProjectGroup1.py
@@ -7,7 +7,9 @@
 # Student 3: 
 # Student 4:
 # DESCRIPTION: Implementation Basic Data Analysys Routines
+from decimal import Rounded
 import time
+from numpy import ones
 import pandas as pd
 # ------ All functions must be implemented by the team, predesigned libraries like pandas (Python) and Daru(Ruby) are allowed. ------
 # Measuring running Times: Implement functions or methods to measure the total runtime as your script runs. All scripts will be run and measured in Odin.
@@ -80,12 +82,45 @@ print(time.process_time()," 4.",severity, "severity is the most common in VA.")
     # 5. What are the 5 cities that had the most accidents in 2019 in California?
 accidentsByYr_St_Cty = dfCopy[['Start_Time','State','City']]
 mostByYr_St_Cty = accidentsByYr_St_Cty[(accidentsByYr_St_Cty['State'] == 'CA') & (accidentsByYr_St_Cty['Start_Time'] == '2019')].value_counts().index[0:5]
-cities = mostByYr_St_Cty.droplevel(['Start_Time', 'State'])
+cities = mostByYr_St_Cty.get_level_values('City')
 print(time.process_time()," 5. The 5 cities that had the most accidents in 2019 in CA are:")
 for i in cities:
     print("\t\t\t\t\t\t\t\t",i)
     # 6. What was the average humidity and average temperature of all accidents of severity 4 that occurred in 2021?
+accidentsByHumid_Temp_Sev_Yr = dfCopy[['Start_Time','Severity','Humidity(%)','Temperature(F)']]
+accidentsByHumid_Temp_Sev_Yr = accidentsByHumid_Temp_Sev_Yr[(accidentsByHumid_Temp_Sev_Yr['Start_Time'] == '2021') & (accidentsByHumid_Temp_Sev_Yr['Severity'] == 4)]
+averageHumidity = accidentsByHumid_Temp_Sev_Yr['Humidity(%)'].mean()
+averageTemperature = accidentsByHumid_Temp_Sev_Yr['Temperature(F)'].mean()
+print(time.process_time()," 6. The average humidity is:", averageHumidity,"% and the average temperature is:", averageTemperature, "F of all accidents of severity 4 that occurred in 2021.")
     # 7. What are the 3 most common weather conditions (weather_conditions) when accidents occurred?
+accidentsByWeather = dfCopy[['Weather_Condition']]
+mostByWeather= accidentsByWeather.value_counts().index[0:3]
+weathers = mostByWeather.get_level_values('Weather_Condition')
+print(time.process_time()," 7. The 3 most common weather conditions when accidents occurred are:")
+for i in weathers:
+    print("\t\t\t\t\t\t\t\t", i)
     # 8. What was the maximum visibility of all accidents of severity 2 that occurred in the state of New Hampshire?
+accidentsByVisibility_Sev_State = dfCopy[['Visibility(mi)', 'Severity', 'State']]
+accidentsByVisibility_Sev_State = accidentsByVisibility_Sev_State[(accidentsByVisibility_Sev_State['State'] == 'NH') & (accidentsByVisibility_Sev_State['Severity'] == 4)]
+maxVisibility = accidentsByVisibility_Sev_State['Visibility(mi)'].max()
+print(time.process_time()," 8. The maximum visibility of all accidents of severity 2 that occurred in the state of New Hampshire is:", maxVisibility,"mi")
     # 9. How many accidents of each severity were recorded in Bakersfield?
+accidentsBySvtyInST = dfCopy[['City','Severity']]
+totalBySvtyInST = accidentsBySvtyInST.loc[(accidentsBySvtyInST['City'] == 'Bakersfield'), 'Severity'].value_counts()
+print(time.process_time()," 9. The total accidents of each severity recorded in Bakersfield are:")
+s = 0
+for i in totalBySvtyInST:
+    print("\t\t\t\t\t\t\t\t\tSeverity", totalBySvtyInST.index[s],":", i)
+    s = s + 1
     # 10. What was the longest accident (in hours) recorded in Florida in the Spring (March, April, and May) of 2022?
+dfCopy = df.copy()
+dfCopy['Start_Time'] = pd.to_datetime(dfCopy['Start_Time'])
+dfCopy['End_Time'] = pd.to_datetime(dfCopy['End_Time'])
+accidentsByTime_ST = dfCopy[['Start_Time','End_Time','State']]
+betweenMonths = accidentsByTime_ST['Start_Time'].dt.month.between(3,5)
+betweenMonths = accidentsByTime_ST[betweenMonths]
+accidentsByTime_ST_Month = betweenMonths[(betweenMonths['State'] == 'FL') & (pd.to_datetime(betweenMonths['Start_Time']).dt.year  == 2022)]
+totalTimeOfAccident = accidentsByTime_ST_Month['End_Time'] - accidentsByTime_ST_Month['Start_Time']
+longestAccident = totalTimeOfAccident.max()
+longestAccidentInSecs = (longestAccident.days*86400) + (longestAccident.seconds)
+print(time.process_time()," 10. The longest accident (in hours) recorded in Flordia in the Spring of 2022 is:", longestAccidentInSecs/(3600), "hours.")
