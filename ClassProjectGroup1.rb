397b5003d8842ee86d381c8118f94649ee5d783e
Marcus Schmidt <mschmidt9@csub.edu>
2022-04-20 14:00:33 -0700

    Ruby data loading and cleaning (inefficient, though)

--- ClassProjectGroup1.rb
+++ ClassProjectGroup1.rb
@@ -3,7 +3,7 @@
 # COURSE: CMPS 3500
 # DATE: 04/06/22
 # Student 1: Elena Castaneda
-# Student 2: 
+# Student 2: Marcus Schmidt
 # Student 3: 
 # Student 4:
 # DESCRIPTION: Implementation Basic Data Analysys Routines
@@ -12,16 +12,56 @@
 # Measuring running Times: Implement functions or methods to measure the total runtime as your script runs. All scripts will be run and measured in Odin.
 # Interfaces: A textual menu should be implemented in a way that the user could load several samples of the same data set (all data cleaning steps would be the same). 
 # During the demo you will be ask to load several data sets with the same structure.
+require "daru"
 
-# Data Loading: Read data from the csv files
-# !!!! We can try https://ankane.org/daru
+# References
+# https://ankane.org/daru
+# https://www.rubydoc.info/gems/daru/Daru/DataFrame
+# https://nbviewer.org/github/SciRuby/sciruby-notebooks/blob/master/Data%20Analysis/Usage%20of%20DataFrame.ipynb
 
-# Data Cleaning: Perform the following cleaning tasks: First load the csv file and store into an array or data frame
-    # 1. Eliminate all rows with data missing in either of the following columns: ID, Severity, zipcpde, Start_Time, End_Time, Visivility(m), Weather_Condition or Country
-    # 2. Eliminate all rows with empty values in 3 or more columns
-    # 3. Eliminate all rows with distance equal to zero
-    # 4. Only consider in your analysis the first 5 digits of the zip code
-    # 5. All accicent that lasted no time (The diference between End_time and Start_time is zero)
+# Data Loading: Read data from the csv file
+print("Loading and cleaning input data set:\n")
+print("************************************\n")
+file = "US_Accidents_data.csv"
+df = Daru::DataFrame.from_csv(file)
+
+=begin
+# Print all of the data frame's column names
+df.vectors.each.with_index do |element, index|
+    puts("#{index}: #{element}")
+end
+=end
+
+puts("Size: #{df.size}")
+
+def nilColumns (row)
+    empty_columns = 0
+    row.each do |element|
+        if element == nil
+            empty_columns += 1
+        end
+    end
+    empty_columns
+end
+
+# Eliminate invalid rows
+df.keep_row_if do |row|
+    row["ID"] != nil && row["Severity"] != nil && row["Zipcode"] != nil && row["Start_Time"] != nil && row["End_Time"] != nil &&
+    row["Distance(mi)"] != 0 &&
+    row["Start_Time"] != row["End_Time"] &&
+    nilColumns(row) < 3
+end
+
+# Clean ZIP codes
+(0..df.size-1).each do |index|
+    row = df.row[index]
+    zip = row["Zipcode"].to_s
+    if zip.length > 5
+        row["Zipcode"] = zip[0, 5]
+    end
+end
+
+puts("Size: #{df.size}")
 
 # Output: Your job is to implement the any functions and methods to answer the following questions about the data set provided:
 # For all these question use Start_Time as the reference date to determine the year and month of the accident.
